072c53f2ce091ca4ca45b3f0acf78a5730b218a8
Daniel Buchner <danieljb2@gmail.com>
2018-10-04 19:45:00 -0700

    WIP

--- global/js/web-keystore.js
+++ global/js/web-keystore.js
@@ -22,6 +22,27 @@
 // 	})
 // }
 
+function sha256(str) {
+  // We transform the string into an arraybuffer.
+  var buffer = new TextEncoder('utf-8').encode(str);
+  return crypto.subtle.digest('SHA-256', buffer).then(function (hash) {
+    return hex(hash);
+  });
+}
+
+function hex(buffer) {
+  var hexCodes = [];
+  var view = new DataView(buffer);
+  for (var i = 0; i < view.byteLength; i += 4) {
+    var value = view.getUint32(i)
+    var stringValue = value.toString(16);
+    var padding = '00000000';
+    var paddedValue = (padding + stringValue).slice(-padding.length);
+    hexCodes.push(paddedValue);
+  }
+  return hexCodes.join("");
+}
+
 async function storageOperation(fn, pubKey) {
 
 	var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
